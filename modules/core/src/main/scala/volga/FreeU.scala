03589581a5952dba2e36f07eb99ce10d67eebdde
Oleg Nizhnikov <odomontois@gmail.com>
2022-12-28 17:00:26 +0100

    @_@_@_@

--- modules/core/src/main/scala/volga/FreeU.scala
+++ modules/core/src/main/scala/volga/FreeU.scala
@@ -1,7 +1,18 @@
 package volga
 
-enum FreeU[+X]:
-  case Obj extends FreeU[tags.Obj[Nothing]]
+sealed trait FreeU[X]
 
-object FreeU:
-  given [A]: FreeU[tags.Obj[A]] = FreeU.Obj
+enum FreeObj[X] extends FreeU[tags.Obj[X]]:
+  case One                                      extends FreeObj[FreeU[tags.One]]
+  case Scala[A]()                               extends FreeObj[FreeU[tags.Scala[A]]]
+  case Prod[A, B](l: FreeObj[A], r: FreeObj[B]) extends FreeObj[FreeU[tags.Tensor[A, B]]]
+
+object FreeU extends ObAliases[FreeU]:
+  given forScala[A]: Ob[$[A]]                                = FreeObj.Scala()
+  given forOne: Ob[I]                                        = FreeObj.One
+  given forTensor[A, B](using a: Ob[A], b: Ob[B]): Ob[A x B] =
+    a match
+      case ao: FreeObj[A] =>
+        b match
+          case bo: FreeObj[B] =>
+            FreeObj.Prod(ao, bo)
--- modules/prob/src/main/scala-3/volga/prob/Mark.scala
+++ modules/prob/src/main/scala-3/volga/prob/Mark.scala
@@ -13,6 +13,7 @@ enum MarkQ[A, B]:
   case Unzip[A, B]()         extends ($[(A, B)] --> ($[A] x $[B]))
   case Split[A, B]()         extends ($[Either[A, B]] --> ($[A] x $[B]))
   case Merge[A, B]()         extends (($[A] x $[B]) --> $[Either[A, B]])
+  case Fail                  extends ($[Unit] --> I)
 
 object MarkQ extends Aliases[MarkQ, FreeU]:
   given HasScalaFunctor[Mark, FreeU] with
@@ -22,18 +23,21 @@ type Mark[A, B] = Mark.Free[A, B]
 
 object Mark extends FreeSMC[MarkQ, FreeU] with Aliases[Mark, FreeU]:
 
-  def unitObj: Ob[I] = FreeU.Obj
+  def unitObj: Ob[I] = FreeU.forOne
 
-  def tensorObj[A: Ob, B: Ob]: Ob[A x B] = FreeU.Obj
+  def tensorObj[A: Ob, B: Ob]: Ob[A x B] = FreeU.forTensor
 
-  def range(x: Int): I --> $[Int]                                      = embed(MarkQ.Range(x))
-  def lift[A, B](f: A => B): $[A] --> $[B]                             = embed(MarkQ.Lift(f))
-  def zip[A, B]: (($[A] x $[B]) --> $[(A, B)])                         = embed(MarkQ.Zip())
-  def unzip[A, B]: ($[(A, B)] --> ($[A] x $[B]))                       = embed(MarkQ.Unzip())
-  def split[A, B]: ($[Either[A, B]] --> ($[A] x $[B]))                 = embed(MarkQ.Split())
-  def merge[A, B]: (($[A] x $[B]) --> $[Either[A, B]])                 = embed(MarkQ.Merge())
+  def range(x: Int): I --> $[Int]                      = embed(MarkQ.Range(x))
+  def lift[A, B](f: A => B): $[A] --> $[B]             = embed(MarkQ.Lift(f))
+  def zip[A, B]: (($[A] x $[B]) --> $[(A, B)])         = embed(MarkQ.Zip())
+  def unzip[A, B]: ($[(A, B)] --> ($[A] x $[B]))       = embed(MarkQ.Unzip())
+  def split[A, B]: ($[Either[A, B]] --> ($[A] x $[B])) = embed(MarkQ.Split())
+  def merge[A, B]: (($[A] x $[B]) --> $[Either[A, B]]) = embed(MarkQ.Merge())
+  def fail: $[Unit] --> I                              = embed(MarkQ.Fail)
+
   def zipWith[A, B, C](f: (A, B) => C): ($[A] x $[B]) --> $[C]         = zip >>> lift(f.tupled)
   def mergeWith[A, B, C](f: A => C, g: B => C): ($[A] x $[B]) --> $[C] = merge >>> lift(_.fold(f, g))
-  val range1                                                           = range(1)
-  val range0                                                           = range(0)
+  def filter[A](p: A => Boolean): $[A] --> $[A]                        =
+    lift((x: A) => if p(x) then Right(x) else Left(())) >>> split >>> (fail >< ident) >>> lconsume
+  val range1                                                           = range(1) // event with guaranteed prob = 1
   val unit: I --> $[Unit]                                              = range1 >>> lift(_ => ())
