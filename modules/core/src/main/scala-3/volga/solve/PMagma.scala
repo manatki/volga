4e6588e4122507a1f7bc07bc2299d6b04823487a
Oleg Nizhnikov <odomontois@gmail.com>
2023-02-02 17:25:50 +0100

    rename some namespaces

--- modules/core/src/main/scala-3/volga/solve/PMagma.scala
+++
@@ -1,9 +0,0 @@
-package volga.solve
-
-trait PMagma[A]:
-    def empty: A
-
-    extension (x: A)
-        def combine(y: A): A
-        final def ##(y: A) = x.combine(y)
-
--- modules/core/src/main/scala-3/volga/syntax/internal/partialFunctions.scala
+++
@@ -1,9 +0,0 @@
-package volga.syntax.internal
-
-extension [A, B](pf: PartialFunction[A, B])
-    def travector: PartialFunction[Iterable[A], Vector[B]] =
-        case xs if xs.forall(pf.isDefinedAt) => xs.iterator.map(pf).toVector
-
-object OfInt:
-    def unapply(s: String): Option[Int] = s.toIntOption
-    
