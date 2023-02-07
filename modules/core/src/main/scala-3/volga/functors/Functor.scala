110cc2b65b0017ea74cd4e94903dd068d0c3f44d
Oleg Nizhnikov <odomontois@gmail.com>
2023-02-07 12:58:50 +0100

    Enum functor derivation

--- modules/core/src/main/scala-3/volga/functors/Functor.scala
+++ modules/core/src/main/scala-3/volga/functors/Functor.scala
@@ -2,34 +2,69 @@ package volga.functors
 
 import compiletime.*
 import deriving.Mirror.{ProductOf, SumOf}
+import scala.reflect.TypeTest
 
+private def subtypeToIntersectionEq[A, B](using ev: A <:< B): A =:= (A & B) =
+    given (A <:< (A & B)) = ev.liftCo[[x] =>> A & x]
+    <:<.antisymm
+
+private def tupleFromProduct[A](a: A)(using m: ProductOf[A])(using A <:< Product): m.MirroredElemTypes =
+    val coerce = subtypeToIntersectionEq[A, Product]
+    type MP[a] = ProductOf[a] { type MirroredElemTypes = m.MirroredElemTypes }
+    val coeMirror = coerce.liftCo[MP](m)
+    Tuple.fromProductTyped[A & Product](coerce(a))(using coeMirror)
+end tupleFromProduct
 trait Functor[F[_]]:
     extension [A](fa: F[A]) def map[B](f: A => B): F[B]
 
 object Functor:
-    inline given derived[T[_] <: Product]: Functor[T] = new:
-        extension [A](fa: T[A])
-            def map[B](f: A => B): T[B] =
-                summonFrom { case ma: ProductOf[T[A]] =>
-                    type TA = ma.MirroredElemTypes
-                    summonFrom { case mb: ProductOf[T[B]] =>
-                        type TB = mb.MirroredElemTypes
-                        val ta = Tuple.fromProductTyped(fa)(using ma)
-                        val tb = functorCall[A, B, TA, TB](ta, f)
-                        mb.fromTuple(tb)
-                    }
-                }
 
-    inline def functorCall[A, B, TA <: Tuple, TB <: Tuple](t: TA, f: A => B): TB =
-        inline t match
+    inline def derived[F[_]]: Functor[F] = new:
+        extension [A](fa: F[A]) def map[B](f: A => B): F[B] = functorCall[A, B, F[A], F[B]](fa, f)
+
+    inline def functorCall[A, B, FA, FB](fa: FA, f: A => B): FB =
+        summonFrom {
+            case ma: ProductOf[FA] =>
+                type TA = ma.MirroredElemTypes
+                summonFrom { case mb: ProductOf[FB] =>
+                    type TB = mb.MirroredElemTypes
+                    val ta = tupleFromProduct(fa)(using ma)(using summonInline[FA <:< Product])
+                    val tb = functorProduct[A, B, TA, TB](ta, f)
+                    mb.fromTuple(tb)
+                }
+            case ma: SumOf[FA]     =>
+                type SA = ma.MirroredElemTypes
+
+                summonFrom { case mb: SumOf[FB] =>
+                    type SB = mb.MirroredElemTypes
+                    functorSum[A, B, FA, FB, SA, SB](fa, f)
+                }
+        }
+
+    inline def functorProduct[A, B, TA <: Tuple, TB <: Tuple](ta: TA, f: A => B): TB =
+        inline ta match
             case EmptyTuple   =>
                 summonInline[EmptyTuple <:< TB](EmptyTuple)
             case t: (a *: ta) =>
                 inline erasedValue[TB] match
                     case _: (b *: tb) =>
-                        val tb: tb = functorCall[A, B, ta, tb](t.tail, f)
+                        val tb: tb = functorProduct[A, B, ta, tb](t.tail, f)
                         val b: b   = summonInline[HeadMatch[a, b, A, B]](t.head)(f)
                         summonInline[(b *: tb) =:= TB](b *: tb)
+
+    inline def functorSum[A, B, FA, FB, SA, SB](pa: FA, f: A => B): FB =
+        inline erasedValue[SA] match
+            case EmptyTuple   =>
+                throw IllegalArgumentException(s"can't match $pa")
+            case t: (a *: ta) =>
+                type Head = a
+                inline erasedValue[SB] match
+                    case _: (b *: tb) =>
+                        summonFrom { case given TypeTest[FA, Head] =>
+                            pa match
+                                case h: Head => summonInline[b <:< FB](functorCall[A, B, a, b](h, f))
+                                case _       => functorSum[A, B, FA, FB, ta, tb](pa, f)
+                        }
 
     given Traverse[[x] =>> x] with Monoidal[[x] =>> x] with
         def pure[A](x: A): A = x
